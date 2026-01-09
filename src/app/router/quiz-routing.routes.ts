1b2484ce07fbb2bd117f50c2e1e6454c1aebd4b3
Marvin Rusinek <marvinrusinek@gmail.com>
2026-01-09 18:57:23 -0500

    cleaned up QuizRouting

--- src/app/router/quiz-routing.routes.ts
+++ src/app/router/quiz-routing.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { IntroductionComponent } from '../containers/introduction/introduction.component';
+import { IntroductionComponent } from
+    '../containers/introduction/introduction.component';
 import { QuizComponent } from '../containers/quiz/quiz.component';
-import { QuizSelectionComponent } from '../containers/quiz-selection/quiz-selection.component';
+import { QuizSelectionComponent } from
+    '../containers/quiz-selection/quiz-selection.component';
 import { ResultsComponent } from '../containers/results/results.component';
 
 import { QuizResolverService } from '../shared/services/quiz-resolver.service';
@@ -12,30 +14,31 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'quiz',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'quiz',
-    component: QuizSelectionComponent,
+    component: QuizSelectionComponent
   },
   {
     path: 'quiz/intro/:quizId',
-    component: IntroductionComponent,
+    component: IntroductionComponent
   },
   {
     path: 'quiz/question/:quizId/:questionIndex',
     component: QuizComponent,
     canActivate: [QuizGuard],
     resolve: { quizData: QuizResolverService },
-    runGuardsAndResolvers: 'always',
+    runGuardsAndResolvers: 'always'
   },
   {
     path: 'quiz/results/:quizId',
-    component: ResultsComponent,
+    component: ResultsComponent
   },
+
   // Backward compatibility redirects
   { path: 'select', redirectTo: 'quiz', pathMatch: 'full' },
   { path: 'intro/:quizId', redirectTo: 'quiz/intro/:quizId', pathMatch: 'full' },
   { path: 'question/:quizId/:questionIndex', redirectTo: 'quiz/question/:quizId/:questionIndex', pathMatch: 'full' },
-  { path: 'results/:quizId', redirectTo: 'quiz/results/:quizId', pathMatch: 'full' },
+  { path: 'results/:quizId', redirectTo: 'quiz/results/:quizId', pathMatch: 'full' }
 ];
