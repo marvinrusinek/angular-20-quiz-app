77228d9aa9ffc21170fdde71dbf863a30aa8f354
marvinrusinek <marvinrusinek@gmail.com>
2026-01-27 16:38:51 -0500

    commit cleanup of IntroductionComponent2

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -10,8 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import {
-  MatSlideToggleChange,
-  MatSlideToggleModule,
+  MatSlideToggleChange, MatSlideToggleModule
 } from '@angular/material/slide-toggle';
 import {
   BehaviorSubject, combineLatest, EMPTY, firstValueFrom, of, Subject
