120c0d08beb5c3922eef7c724fd160b714b32f6c
null <null>
2026-03-25 15:36:59 -0400

    commit option-utils again

--- src/app/shared/utils/option-utils.ts
+++ src/app/shared/utils/option-utils.ts
@@ -1,10 +1,7 @@
 import { Option } from '../models/Option.model';
 
 export function isValidOption(option: Option): boolean {
-  return (
-    option &&
+  return option &&
     typeof option === 'object' &&
-    'text' in option &&
-    'correct' in option
-  );
+    'text' in option;
 }
