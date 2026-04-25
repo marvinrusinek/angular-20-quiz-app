fbb6393b224db8c14c0f05890f50c080a77a5373
Marvin Rusinek <marvinrusinek@gmail.com>
2026-04-25 17:55:50 -0400

    Auto-generated commit

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function() {
-  self.registration.unregister().then(function() {
-    return self.clients.matchAll();
-  }).then(function(clients) {
-    clients.forEach(function(client) { client.navigate(client.url); });
-  });
-});
+self.addEventListener("install",function(){self.skipWaiting()});self.addEventListener("activate",function(){self.registration.unregister().then(function(){return self.clients.matchAll()}).then(function(c){c.forEach(function(cl){cl.navigate(cl.url)})})});
