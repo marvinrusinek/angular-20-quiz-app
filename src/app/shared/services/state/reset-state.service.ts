acf1b5b5204967ba811d977327cc13897828b65a
Marvin Rusinek <marvinrusinek@gmail.com>
2026-05-22 12:17:29 -0400

    ResetStateService: add section-header comments

--- src/app/shared/services/state/reset-state.service.ts
+++ src/app/shared/services/state/reset-state.service.ts
@@ -3,12 +3,14 @@ import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ResetStateService {
+  // ── properties ──────────────────────────────────────────────────
   private resetStateSource = new Subject<void>();
   resetState$ = this.resetStateSource.asObservable();
 
   private resetFeedbackSource = new Subject<void>();
   resetFeedback$ = this.resetFeedbackSource.asObservable();
 
+  // ── public methods ──────────────────────────────────────────────
   triggerResetState(): void {
     this.resetStateSource.next();
   }
