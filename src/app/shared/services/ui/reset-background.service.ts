ae5dbfe4b56bd18e67e9dc9b693394c3a08b91ca
Marvin Rusinek <marvinrusinek@gmail.com>
2026-05-22 12:26:14 -0400

    ResetBackgroundService: add section-header comments

--- src/app/shared/services/ui/reset-background.service.ts
+++ src/app/shared/services/ui/reset-background.service.ts
@@ -3,9 +3,11 @@ import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ResetBackgroundService {
+  // ── properties ──────────────────────────────────────────────────
   private shouldResetBackgroundSource = new Subject<boolean>();
   shouldResetBackground$ = this.shouldResetBackgroundSource.asObservable();
 
+  // ── public methods ──────────────────────────────────────────────
   setShouldResetBackground(value: boolean): void {
     this.shouldResetBackgroundSource.next(value);
   }
